6d6ac8505ed7e612f846d30b3015350fd79a151d
Daniel Shafer <github@danielshafer.com>
2016-12-29 12:10:45 -0500

    add the first couple exercises

--- 02OddOccurencesInArray.js
+++ 02OddOccurencesInArray.js
@@ -2,7 +2,6 @@ function solution(A) {
     if((A === undefined) || (typeof A !== "object") || (A === [])) {
         return;
     };
-    // write your code in JavaScript (Node.js 6.4.0)
     var occurrences = {};
     A.forEach(function(i){
         if(occurrences[i] === undefined) {
