04d7c65542763ebc8768dd28f9275abd19c65ab8
Daniel Shafer <github@danielshafer.com>
2016-12-29 19:23:54 -0500

    add comments

--- 01CyclicRotation.js
+++ 01CyclicRotation.js
@@ -1,7 +1,18 @@
+// PSEUDOCODE:
+// Write a function that takes an array and rotates it a give number of times
+// Two inputs: array and number of times to rotate
+// Output: the rotated array
+// It would be cool to have an escape for some of the common edge cases like if someone passes an empty array or nothing at all. 
+// Rotate means to take last element off the end and put it on the front of the array. 
+// We have to figure out two things, first, how to rotate an array and second, how to do that the number of times that is getting passed.
+// The loop part is easier because you can just make a block that runs that particular number of times that gets passed. 
+// The rotate needs a couple things:
+// 	First, it removes from the end and stores it as a placeholder varaiable which can be used subsequently. 
+// 	Second, it pushes that value onto the front of the array. 
+// Lastly, the solution function returns the completed array. 
+
 function solution(A, K) {
-    if((A === undefined) || (K === undefined) || (A === [])) {
-        return;
-    };
+    if((A === undefined) || (K === undefined) || (A === [])) { return; };
     while(K--) {
         var placeholder = (A.pop());
         A.unshift(placeholder);
--- 02OddOccurencesInArray.js
+++ 02OddOccurencesInArray.js
@@ -1,7 +1,15 @@
+// PSEUDOCODE:
+// Write a function that takes an array and finds the one that doesn't show up twice. 
+// One input: an array of numbers
+// Output: a number 
+// There should also be some escapes to take care of edge cases like if someone passes something other than an array, or an array, or nothing at all. 
+// We need to make an occurrence hash which will show how many times some number shows up. 
+// We then need to go through each of the numbers and for each one, add it to the occurence hash with an occurence of one. 
+// But if it already exists in the hash, we can delete the record entirely beause the full pair will have been accounted before. 
+// The only value left in the occurence hash by the end is the answer because it's the only one that was added and not removed later. 
+
 function solution(A) {
-    if((A === undefined) || (typeof A !== "object") || (A === [])) {
-        return;
-    };
+    if((A === undefined) || (typeof A !== "object") || (A === [])) { return; };
     var occurrences = {};
     A.forEach(function(i){
         if(occurrences[i] === undefined) {
