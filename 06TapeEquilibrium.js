a8e2ffc73a77972c3ce08201ccb112194c2837ed
Daniel Shafer <github@danielshafer.com>
2016-12-31 11:32:59 -0800

    06tapeequilibrium

---
+++ 06TapeEquilibrium.js
@@ -0,0 +1,26 @@
+// PESUDOCODE:
+// Write a function that finds the smallest difference between two sides of a tape, which is an array of numbers.
+// The right side of the tape is subtracted from the left. 
+// The value of the selected index is added to the left. 
+// Input is an array of numbers.
+// Output is the value of the smallest difference between left and right. 
+// We need a loop that gives each value in the array the opportunity to be the dividing point between left and right. 
+// We need a way to add the values in the left and the values in the right and subtract those sums from each other. 
+
+function solution(A) {
+    var smallest = 100;
+    for (var i = 0; i < A.length - 1; i++){
+        var left = A.slice(0, (i + 1));
+        var leftSum = left.reduce(add, 0)
+        var right = A.slice((i + 1), (A.length));
+        var rightSum = right.reduce(add, 0);        
+        if(Math.abs(leftSum - rightSum) < smallest) {
+            smallest = Math.abs(leftSum - rightSum);
+        };
+    };
+    return smallest;
+}
+
+function add(a, b) {
+    return a + b;
+}
